f7fcc663bdafb9d2cad6c109765c28978f7ed71e
Evanp0501 <otsu.futa@gmail.com>
2026-03-18 13:31:23 +0900

    feat:enemy
    プレイヤーに向かって攻撃を行う敵を追加
    弾の速度や移動幅のはinspecterで制御可能

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifeTime = 3f; // ← 何秒で消えるか
+
+    void Start()
+    {
+        // 一定時間後に自動で削除
+        Destroy(gameObject, lifeTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // プレイヤーならヒット
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("プレイヤーにヒット！");
+        }
+
+        // 当たったら消える
+        Destroy(gameObject);
+    }
+}
