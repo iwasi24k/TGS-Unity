714026bf2369489529042b42d1b2ac11364f6a92
YUKU <f29943862@gmail.com>
2026-04-14 16:47:19 +0900

    update: SC_EnemyBlownAway調整中
    SC_EnemyBlownAway調整中

--- Assets/Scripts/Enemy/SC_EnemyBlownAway.cs
+++ Assets/Scripts/Enemy/SC_EnemyBlownAway.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public class SC_EnemyBlownAway : SC_EnemyBaceState
 {
     [Header("Settings")]
-    [Tooltip("吹き飛ばされる基礎距離"), SerializeField] private float blownAwayDistance = 5f;
+    [Tooltip("吹き飛ばされる力"), SerializeField] private float blownAwayPower = 5f;
+    [Tooltip("吹き飛ばされる反作用力"), SerializeField] private float blownAwayReactionPower = 2f;
+    [Tooltip("吹き飛ばされる方向"), SerializeField] private Vector3 blownAwayDirection = new Vector3(0, 0, 0);
+    [Tooltip("力の減衰速度"), SerializeField] private float decaySpeed = 5f;
 
     public override void Enter(SC_EnemyStatusManager owner)
     {
@@ -19,5 +22,38 @@ public class SC_EnemyBlownAway : SC_EnemyBaceState
     public override void UpdateState(SC_EnemyStatusManager owner)
     {
         Debug.Log("BlownAway State Update");
+
+        blownAwayPower -= decaySpeed * Time.deltaTime;
+
+        if (blownAwayPower < 0f)
+        {
+            blownAwayPower = 0f;
+        }
+
+        owner.transform.position += blownAwayDirection * blownAwayPower * Time.deltaTime;
+
+        if (blownAwayPower <= 0f)
+        {
+            owner.
+        }
+    }
+
+    // 吹き飛ばされる力を設定するメソッド
+    public void SetPower(float power)
+    {
+        blownAwayPower = power;
+    }
+
+    // 吹き飛ばされる方向を設定するメソッド
+    public void SetDirection(Vector3 direction)
+    {
+        blownAwayDirection = direction.normalized;
+    }
+
+    // 吹き飛ばされる力と方向を同時に設定するメソッド
+    public void SetBlownAway(float power, Vector3 direction)
+    {
+        blownAwayPower = power;
+        blownAwayDirection = direction.normalized;
     }
 }
