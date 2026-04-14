61ee5f5f8b58aa162a5ef4d82fe6dd91c6e76aeb
Evanp0501 <otsu.futa@gmail.com>
2026-04-14 17:40:15 +0900

    feat:enemystate_move
    ランダム方向に移動するようにしました

--- Assets/Scripts/Enemy/SC_EnemyMove.cs
+++ Assets/Scripts/Enemy/SC_EnemyMove.cs
@@ -1,3 +1,4 @@
+using UnityEditorInternal;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Enemy/Move State")]
@@ -5,10 +6,26 @@ public class SC_EnemyMove : SC_EnemyBaceState
 {
     [Header("Settings")]
     [Tooltip("移動速度"), SerializeField] private int moveSpeed = 3;
+    [Tooltip("移動距離"), SerializeField] private float moveDistance = 3f;
+
+    private Vector3 moveDirection;
+    private Vector3 startPosition;
+    private Rigidbody rb;
 
     public override void Enter(GameObject Owner, SC_EnemyStatusManager Manager)
     {
         Debug.Log("Move State Enter");
+
+        rb = Owner.GetComponent<Rigidbody>();
+
+        // 開始位置記録
+        startPosition = Owner.transform.position;
+
+        // ランダム方向（XZ平面）
+        moveDirection = new Vector3
+            (
+            Random.Range(-1f, 1f),0f,Random.Range(-1f, 1f)
+            ).normalized;
     }
 
     public override void Exit(GameObject Owner, SC_EnemyStatusManager Manager)
@@ -19,5 +36,31 @@ public class SC_EnemyMove : SC_EnemyBaceState
     public override void UpdateState(GameObject Owner, SC_EnemyStatusManager Manager)
     {
         Debug.Log("Move State Update");
+
+        if (rb == null) return;
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(player == null) return;
+
+        Vector3 direction = player.transform.position - Owner.transform.position;
+        direction.y = 0f;
+
+        // 向き変更
+        rb.MoveRotation(Quaternion.LookRotation(direction));
+
+        // velocityで移動
+        rb.linearVelocity = moveDirection * moveSpeed;
+
+        // 移動距離チェック
+        float distance = Vector3.Distance(startPosition, Owner.transform.position);
+
+        if (distance >= moveDistance)
+        {
+            // 停止
+            rb.linearVelocity = Vector3.zero;
+
+            // 次のステートへ
+            Manager.TransitionToNext();
+        }
     }
 }
