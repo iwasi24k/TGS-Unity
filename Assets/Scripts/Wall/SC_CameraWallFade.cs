9c7cb56c2ce7ae785ce5b55caa7ec498d6a565ff
iwasi24k <iwasi.ais.2472@gmail.com>
2026-05-19 17:20:58 +0900

    CameraWallFadeスクリプトの中身が消えていたため修正

--- Assets/Scripts/Wall/SC_CameraWallFade.cs
+++ Assets/Scripts/Wall/SC_CameraWallFade.cs
@@ -1,16 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class SC_CameraWall : MonoBehaviour
+public class CameraWallFade : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform target;
+
+    private List<SC_FadeObject> fadedObjects = new List<SC_FadeObject>();
+
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        // ëOÉtÉåÅ[ÉÄÇÃìßñæâªâèú
+        foreach (SC_FadeObject obj in fadedObjects)
+        {
+            obj.FadeIn();
+        }
+
+        fadedObjects.Clear();
+
+        Vector3 dir = target.position - transform.position;
+        float distance = dir.magnitude;
+
+        RaycastHit[] hits = Physics.RaycastAll(
+            transform.position,
+            dir.normalized,
+            distance
+        );
+
+        foreach (RaycastHit hit in hits)
+        {
+            SC_FadeObject fadeObj = hit.collider.GetComponent<SC_FadeObject>();
+
+            if (fadeObj != null)
+            {
+                fadeObj.FadeOut();
+                fadedObjects.Add(fadeObj);
+            }
+        }
     }
 }
