689b0d366fe8cb46bd651cc266797d484b3c6b97
pawn16124 <kento.monkey124@gmail.com>
2026-06-10 11:17:55 +0900

    del:不要なデータのリムーブ
    デベロップ統合時の不要データを削除

--- Assets/Scripts/Player/StateMachene/SC_PlayerWeakAttackState.cs
+++ Assets/Scripts/Player/StateMachene/SC_PlayerWeakAttackState.cs
@@ -50,23 +50,41 @@ public class SC_PlayerWeakAttackState : SC_PlayerBaseState
         {
 
             GameObject[] targets;
-
-            // AttackArea
-            targets = AttackManager.GetInAreaObjectByTag("Enemy");
-            if (targets == null || targets.Length == 0) return;
-
             bool isHit = false;
-
-            foreach (var target in targets)
             {
-                if (target == null) continue; // ここが重要：null 要素をスキップ
+                // AttackArea
+                targets = AttackManager.GetInAreaObjectByTag("Enemy");
+                if (targets == null || targets.Length == 0) return;
 
-                var Enemy = target.GetComponent<SC_EnemyStatusManager>();
-                if (Enemy == null) continue;
 
-                // ダメージ処理など
-                Enemy.TakeDamage(AttackManager.GetWeakDamage(), owner.transform.position, false, AttackType.Weak1);
-                isHit = true;
+                foreach (var target in targets)
+                {
+                    if (target == null) continue; // ここが重要：null 要素をスキップ
+
+                    var Enemy = target.GetComponent<SC_EnemyStatusManager>();
+                    if (Enemy == null) continue;
+
+                    // ダメージ処理など
+                    Enemy.TakeDamage(AttackManager.GetWeakDamage(), owner.transform.position, false, AttackType.Weak1);
+                    isHit = true;
+                }
+            }
+
+            {
+                targets = AttackManager.GetInAreaObjectByTag("Bullet");
+                if (targets == null || targets.Length == 0) return;
+
+                foreach (var target in targets)
+                {
+                    if (target == null) continue; // ここが重要：null 要素をスキップ
+
+                    var Missile = target.GetComponent<SC_ReflectableMissile>();
+                    if (Missile == null) continue;
+
+                    // ダメージ処理など
+                    Missile.ReflectByPlayer(owner.transform);
+                    isHit = true;
+                }
             }
 
             if (isHit)
