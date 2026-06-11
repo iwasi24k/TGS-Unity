cfad16d14684681f0cc9e8dcec01add313a56ae8
pawn16124 <kento.monkey124@gmail.com>
2026-06-11 18:07:27 +0900

    PlayerUIのプレファブ化とフィールドの一部修正
    SC_PlayerUIにHPPointPrefubとPointAliveSprite,PointDeadSpriteに必要なものをアタッチしてください

--- Assets/Scripts/Player/SC_PlayerUI.cs
+++ Assets/Scripts/Player/SC_PlayerUI.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class SC_PlayerUI : MonoBehaviour
 {
-    [SerializeField] private SC_PlayerHP OwnerHP;
-    [SerializeField] private Slider hpSlider;
-
-    void Start()
+    [Header("Player HP")]
+    [SerializeField] private Image hpPointPrefab;
+    [SerializeField] private Sprite hpPointAlive;
+    [SerializeField] private Sprite hpPointDead;
+    
+    private Image[] hpPoints;
+    public void InitializeHPUI(SC_PlayerHP OwnerHP)
     {
-
+        //HPÇÃUIÇèâä˙âª
+        hpPoints = new Image[OwnerHP.GetMaxHP()];
+        if (hpPointPrefab != null)
+        {
+            for (int i = 0; i < OwnerHP.GetMaxHP(); i++)
+            {
+                //HPÇÃUIÇê∂ê¨
+                hpPoints[i] = Instantiate(hpPointPrefab, transform);
+            }
+        }
     }
 
-    void Update()
+    public void UpdateHPUI(SC_PlayerHP OwnerHP)
     {
-        
+        for (int i = 0; i < OwnerHP.GetMaxHP(); i++)
+        {
+            if (i < OwnerHP.GetCurrentHP())
+            {
+                hpPoints[i].sprite = hpPointAlive;
+            }
+            else
+            {
+                hpPoints[i].sprite = hpPointDead;
+            }
+        }
     }
+
 }
