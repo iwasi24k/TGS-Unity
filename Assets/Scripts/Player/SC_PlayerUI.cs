f85ab70060634b12c8717cf3799e982f88dfdf52
pawn16124 <kento.monkey124@gmail.com>
2026-06-11 16:22:35 +0900

    feat:PlayerUI
    UI関連のスクリプトデータ

---
+++ Assets/Scripts/Player/SC_PlayerUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SC_PlayerUI : MonoBehaviour
+{
+    [SerializeField] private SC_PlayerHP OwnerHP;
+    [SerializeField] private Slider hpSlider;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
